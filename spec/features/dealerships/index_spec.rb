258d24bb1a5d3f019f0c3f833ee5038f6c66a2fc
Katie Lonsdale <katie952@gmail.com>
2023-03-21 20:44:29 -0600

    add dealerships index page

---
+++ spec/features/dealerships/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+# For each parent table
+# As a visitor
+# When I visit '/dealerships'
+# Then I see the name of each parent record in the system
+
+
+RSpec.describe "/dealerships", type: :feature do
+  describe "as a visitor, when I visit the dealership index page" do
+    it "should display the name of each dealerships" do
+      dealership_1 = Dealership.create!(name: "Mountain States Toyota", financing_available: true, employees: 100)
+      dealership_2 = Dealership.create!(name: "Stevinson Automotive", financing_available: true, employees: 300)
+      dealership_3 = Dealership.create!(name: "Mom&Pop Auto Shop", financing_available: false, employees: 2)
+      visit "/dealerships"
+
+      expect(page).to have_content(dealership_1.name)
+      expect(page).to have_content(dealership_2.name)
+      expect(page).to have_content(dealership_3.name)
+    end
+  end
+
+end
--- spec/models/dealership_spec.rb
+++ spec/models/dealership_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Dealership, type: :model do
   # describe 'relationships' do
   #   it { should have_many :cars }
   # end
+
+
 end
