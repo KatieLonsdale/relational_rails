7f2e564e26f2d59b995d95411ea654b6e7666871
Katie Lonsdale <katie952@gmail.com>
2023-03-22 11:43:32 -0600

    test: add show action test

---
+++ spec/features/cars/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+# User Story 4, Child Show 
+
+# As a visitor
+# When I visit '/child_table_name/:id'
+# Then I see the child with that id including the child's attributes
+# (data from each column that is on the child table)
+
+RSpec.describe "/cars/:id", type: :feature do
+  describe "as a visitor, when I visit the car show page" do
+    it "should display car with that id and its attributes" do
+      dealership_1 = Dealership.create!(name: "Mountain States Toyota", financing_available: true, employees: 100)
+      dealership_2 = Dealership.create!(name: "Stevinson Automotive", financing_available: true, employees: 300)
+      dealership_3 = Dealership.create!(name: "Mom&Pop Auto Shop", financing_available: false, employees: 2)
+
+      car_1 = Car.create!(make: 'Toyota', model: 'Corolla', awd: false, mileage: 30200, dealership_id: dealership_1.id)
+      car_2 = Car.create!(make: 'Nissan', model: 'Rogue', awd: true, mileage: 46414, dealership_id: dealership_2.id)
+      car_3 = Car.create!(make: 'Porsche', model: '911', awd: false, mileage: 160234, dealership_id: dealership_3.id)
+      visit "/cars/car_1.id"
+      save_and_open_page
+
+      expect(page).to have_content(car_1.make)
+      expect(page).to have_content(car_1.model)
+      expect(page).to have_content(car_1.awd)
+      expect(page).to have_content(car_1.mileage)
+
+      # expect(page).to have_content(car_2.make)
+      # expect(page).to have_content(car_2.model)
+      # expect(page).to have_content(car_2.awd)
+      # expect(page).to have_content(car_2.mileage)
+
+      # expect(page).to have_content(car_3.make)
+      # expect(page).to have_content(car_3.model)
+      # expect(page).to have_content(car_3.awd)
+      # expect(page).to have_content(car_3.mileage)
+    end
+  end
+end
