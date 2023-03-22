36a2dd839aa2fe3c2d988448ce7b380c59dc2973
Katie Lonsdale <katie952@gmail.com>
2023-03-22 11:06:51 -0600

    add relationship tests for car and dealership models

--- spec/models/dealership_spec.rb
+++ spec/models/dealership_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Dealership, type: :model do
-  # describe 'relationships' do
-  #   it { should have_many :cars }
-  # end
-
-
+  describe 'relationships' do
+    it { should have_many :cars }
+  end
 end
