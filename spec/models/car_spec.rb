b8e6436ee1e23583e5f7891ca0db6476e23adff3
Katie Lonsdale <katie952@gmail.com>
2023-03-21 20:06:43 -0600

    add car and dealership test files and models

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  describe 'relationships' do
-    it {should belong_to :dealership}
-  end
+  # describe 'relationships' do
+  #   it { should belong_to(:dealership) }
+  # end
 end
