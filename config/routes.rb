f596a989e2f688758d9c3f3707adcb01fce1d446
Katie Lonsdale <katie952@gmail.com>
2023-03-22 16:17:27 -0600

    add route for dealership/cars index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/dealerships/:id', to: 'dealerships#show'
   get '/cars', to: 'cars#index'
   get '/cars/:id', to: 'cars#show'
+  get '/dealerships/:dealership_id/cars', to: 'dealerships/cars#index'
 end
