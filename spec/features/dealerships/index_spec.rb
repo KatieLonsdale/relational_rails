d2a99e4bb8797b62be9ea1b2de4860d28f6dfd89
Katie Lonsdale <katie952@gmail.com>
2023-03-23 14:08:02 -0600

    test: add ordered by created test

--- spec/features/dealerships/index_spec.rb
+++ spec/features/dealerships/index_spec.rb
@@ -2,13 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "/dealerships", type: :feature do
   describe "as a visitor, when I visit the dealership index page" do
-    let(:dealer1) { "Mountain States Toyota" }
-    let(:dealer2) { "Stevinson Automotive" }
-    let(:dealer3) { "Mom&Pop Auto Shop" }
     before :each do 
       @dealership_1 = Dealership.create!(name: "Mountain States Toyota", financing_available: true, employees: 100)
       @dealership_2 = Dealership.create!(name: "Stevinson Automotive", financing_available: true, employees: 300)
-      @dealership_3 = Dealership.create!(name: "Mom&Pop Auto Shop", financing_available: false, employees: 2)
+      @dealership_3 = Dealership.create!(name: "Mom and Pop Auto Shop", financing_available: false, employees: 2)
       visit "/dealerships"
     end
     it "should display the name of each dealerships" do
@@ -17,15 +14,12 @@ RSpec.describe "/dealerships", type: :feature do
       expect(page).to have_content(@dealership_3.name)
     end
 
-    xit "should be ordered starting with most recently created" do
-      save_and_open_page
-
-      expect(:dealer3).to appear_before(:dealer2)
-      expect(:dealer2).to appear_before(:dealer1)
+    it "should be ordered starting with most recently created" do
+      expect(@dealership_3.name).to appear_before(@dealership_2.name)
+      expect(@dealership_2.name).to appear_before(@dealership_1.name)
     end
 
     it "should display created at timestamps" do
-      save_and_open_page
       expect(page).to have_content(@dealership_1.created_at)
       expect(page).to have_content(@dealership_2.created_at)
       expect(page).to have_content(@dealership_3.created_at)
