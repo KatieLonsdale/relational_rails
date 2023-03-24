ba242480d087fc11d37c505255cb5981c5d2a0cc
Katie Lonsdale <katie952@gmail.com>
2023-03-24 17:53:44 -0600

    feat: add edit and update methods

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,20 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  def edit
+    @car=Car.find(params[:id])
+  end
+
+  def update
+    car = Car.find(params[:car_id])
+    car.update({
+    make: params[:make],
+    model: params[:model],
+    awd: params[:awd],
+    mileage: params[:mileage]
+    })
+    car.save
+    redirect_to "/cars/#{car.id}"
+  end
 end
