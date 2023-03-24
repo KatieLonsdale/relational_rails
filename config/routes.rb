29db8d72f61ab8dd01ac737383def590b78e0147
Katie Lonsdale <katie952@gmail.com>
2023-03-23 20:19:35 -0600

    add dealerships/new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/dealerships', to: 'dealerships#index'
-  get '/dealerships/:id', to: 'dealerships#show'
+  get 'dealerships/new', to: 'dealerships#new'
   get '/cars', to: 'cars#index'
-  get '/cars/:id', to: 'cars#show'
   get '/dealerships/:dealership_id/cars', to: 'dealerships/cars#index'
+  get '/cars/:id', to: 'cars#show'
+  get '/dealerships/:id', to: 'dealerships#show'
 end
