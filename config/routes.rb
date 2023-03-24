a447cc7f5ea2296df02e28a1375502e14a64d528
Katie Lonsdale <katie952@gmail.com>
2023-03-24 17:53:02 -0600

    add routes to edit and update car

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   get 'dealerships/:dealership_id/edit', to: 'dealerships#edit'
   get 'dealerships/:dealership_id/cars/new', to: 'dealerships/cars#new'
   get '/cars', to: 'cars#index'
+  get '/cars/:id/edit', to: 'cars#edit'
   get '/dealerships/:dealership_id/cars', to: 'dealerships/cars#index'
   get '/cars/:id', to: 'cars#show'
   get '/dealerships/:id', to: 'dealerships#show'
   post '/dealerships', to: 'dealerships#create'
   post '/dealerships/:dealership_id/cars', to: 'dealerships/cars#create'
   patch '/dealerships/:dealership_id', to: 'dealerships#update'
+  patch '/cars/:car_id', to: 'cars#update'
 end
