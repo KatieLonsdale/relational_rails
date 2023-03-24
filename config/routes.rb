daebb9e05f4671792910c6882668cbb614bfbc92
Katie Lonsdale <katie952@gmail.com>
2023-03-23 20:46:08 -0600

    add post route for creating dealership

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/dealerships/:dealership_id/cars', to: 'dealerships/cars#index'
   get '/cars/:id', to: 'cars#show'
   get '/dealerships/:id', to: 'dealerships#show'
+  post '/dealerships', to: 'dealerships#create'
 end
