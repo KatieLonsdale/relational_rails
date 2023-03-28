325902ee7bba2dd44c2616adead7df08e88890bd
Katie Lonsdale <katie952@gmail.com>
2023-03-28 13:48:52 -0600

    refactor: remove redundancy from methods

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,15 +2,15 @@ class Car < ApplicationRecord
   belongs_to :dealership
 
   def self.cars_with_awd
-    Car.where(awd: true)
+    where(awd: true)
   end
 
   def self.sort_alphabetically
-    Car.order(:make, :model)
+    order(:make, :model)
   end
 
   def self.filter_by_mileage(mileage)
     miles = mileage.delete(',').to_i
-    Car.where("mileage > #{miles}")
+    where("mileage > #{miles}")
   end
 end
